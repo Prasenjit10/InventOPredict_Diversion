5badab385d85a8e54dda5c661eebd93af3f87638
Ahana Dasgupta <ahanadasgupta26@gmail.com>
2026-02-28 16:28:06 +0530

    updates in app

--- backend/app.py
+++ backend/app.py
@@ -28,3 +28,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    company_name = db.Column(db.String(150), nullable=False)
+    company_code = db.Column(db.String(50), unique=True, nullable=False)
+
+    warehouse_name = db.Column(db.String(150), nullable=False)
+    warehouse_location = db.Column(db.String(200), nullable=False)
+    warehouse_code = db.Column(db.String(50), unique=True, nullable=False)
+
+    password = db.Column(db.String(200), nullable=False)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    experience = db.Column(db.Text, nullable=False)
+
+
+class StockoutReminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    product_name = db.Column(db.String(150), nullable=False)
+    stockout_date = db.Column(db.Date, nullable=False)
+    reminder_stage = db.Column(db.Integer, default=0)
+
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
